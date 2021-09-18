ca401c045629a4f9b0ce563f8fb4848e0c304fe3
Vinicius Rezende <rezendeveloper@gmail.com>
2021-09-18 19:46:49 -0300

    Handle Mix and Watch Later lists

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -36,6 +36,9 @@ export const getNickname = async (message: Message) => {
 export const getPlaylistId = (url: string) => {
   try {
     const id = new URL(url).searchParams.get('list')
+    const isMix = id?.startsWith('RD')
+    const isWatchLater = id?.startsWith('TLPQMT')
+    if (isMix || isWatchLater) return undefined
     return id
   } catch (err) {
     return undefined
